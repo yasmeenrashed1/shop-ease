3087e81556be1528cd79b81883fc252aeb342e07
Yasmeen Muhamed <yasmeenmsf2@gmail.com>
2025-09-02 16:30:07 +0300

    Switch to Hash Mode for Vue Router

--- my-vue-app/src/router/index.js
+++ my-vue-app/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter,createWebHashHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import About from '../pages/About.vue'
 import Products from '../pages/Products.vue'
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history:  createWebHashHistory(),
   routes
 })
 
